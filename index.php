15a9e1b1939ad02d969be8b10f28d72ba5998bd0
MiguelEM <jose.miguel.7897763@gmail.com>
2024-12-04 23:50:44 -0600

    REVISALO CHRIS

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-  //Instancia de la session
-  session_start();
+//TUPU
+//Instancia de la session
+session_start();
 
-  if(!isset($_SESSION["is_logged"]) || ($_SESSION["is_logged"] == true)){
-    //retorna al dashboard
-    if(isset($_SESSION["id_rol"])){
-      if($_SESSION["id_rol"] == 128 || $_SESSION["id_rol"] == 85){
-        header('Location: ../panel/dashboard.php');
-        exit();
-      }//end if id_rol
-    }//end if isset
-  }//end 
-  else{
-    session_destroy();
-  }//end else
+if (!isset($_SESSION["is_logged"]) || ($_SESSION["is_logged"] == true)) {
+  //retorna al dashboard
+  if (isset($_SESSION["id_rol"])) {
+    if ($_SESSION["id_rol"] == 128 || $_SESSION["id_rol"] == 85) {
+      header('Location: ../panel/dashboard.php');
+      exit();
+    }//end if id_rol
+  }//end if isset
+}//end 
+else {
+  session_destroy();
+}//end else
 ?>
 
 Aquí tendran que colocar su página principal donde hablan sobre el sistema de mtvaward
@@ -25,9 +26,11 @@ Para ello, se solicita que deben contener lo siguiente
   <li>Artista</li>
   <li><a href="./app/views/portal/mi_perfil.php">Mi perfil</a></li>
   <li>Votar (Se muestra cuando esta inciiado la asesión)</li>
-  <li><a href="<?= (isset($_SESSION['is_logged'])) ? './app/backend/panel/liberate_user.php' :  './app/views/usuario/login.php' ?>"><?= (isset($_SESSION['is_logged'])) ? 'Logout' :  'Login' ?></a></li>
-<ul>
-<!-- * Inicio
+  <li><a
+      href="<?= (isset($_SESSION['is_logged'])) ? './app/backend/panel/liberate_user.php' : './app/views/usuario/login.php' ?>"><?= (isset($_SESSION['is_logged'])) ? 'Logout' : 'Login' ?></a>
+  </li>
+  <ul>
+    <!-- * Inicio
 * Información sobre el evento
 * Generos
   Mostrar todos los generos disponibles y habilitados
@@ -41,5 +44,3 @@ Para ello, se solicita que deben contener lo siguiente
   Aqui tienen que realizar una clasificacion de los votos ya sean por album o por artista y deben de mostrar el count de ello
 * Login
   Aqui debe de estar la opción de redireccioanmiento del login, recordando si es un administrador o artista se redirecciona al panel administrable en caso contrario al portal -->
-
-  
